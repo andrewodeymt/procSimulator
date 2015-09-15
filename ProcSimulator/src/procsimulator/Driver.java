66184885516505e36c9ca01c93502747141ef153
andy <andrew.odonnell91@gmail.com>
2015-09-15 16:52:20 -0600

    Initial Commit

---
+++ ProcSimulator/src/procsimulator/Driver.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package procsimulator;
+
+/**
+ *
+ * @author andy
+ */
+public class Driver {
+    public static void main(String[] args) {
+        ProcSimulator proc1 = new ProcSimulator();
+        
+    }
+}
