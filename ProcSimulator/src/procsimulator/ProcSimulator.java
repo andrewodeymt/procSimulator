7b3010da7b6e979765ba6f55dffa0f150f23899b
andrewodeymt <andrewodeymt@users.noreply.github.com>
2015-09-15 19:42:56 -0600

    Update ProcSimulator.java

--- ProcSimulator/src/procsimulator/ProcSimulator.java
+++ ProcSimulator/src/procsimulator/ProcSimulator.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 
 public class ProcSimulator {
     
+    int m, n = 0;
     int size = 1000;
     int minPrTime = 1;
     int maxPrTime = 500;
@@ -21,22 +22,37 @@ public class ProcSimulator {
     int maxArrTime = 1000;
     int[] procTime = new int[size];
     int[] arrTime = new int[size];
-    int[][] processor = new int[size][size];
     Random r  = new Random();
     int gid;
     int procCount;
-    int totalProcTime = 0;
+    int totalProcTime1, totalProcTime2 = 0;
+    
+   private int proc1(int arrTime, int procTime)
+   {
+       totalProcTime1 = (procTime-arrTime);
+       return totalProcTime1;
+       
+       System.out.println("Proc 1 Start Time: "+arrTime+", End Time: "+procTime);
+   }
+   
+   private int proc2(int arrTime, int procTime)
+   {
+       totalProcTime2 = (procTime-arrTime);
+       return totalProcTime2;
+   }    
     
     //Constructor
-    public ProcSimulator(){
-               
+    public ProcSimulator()
+    {
+    
+        
         //intro
         System.out.println("Please enter your GID: ");
 
         //scanner for user input
         Scanner sc  = new Scanner(System.in);
-        int gid = sc.nextInt();
-        int procCount = ((gid%3)+2);
+        gid = sc.nextInt();
+        procCount = ((gid%3)+2);
     
         System.out.println(procCount);
     
@@ -44,7 +60,6 @@ public class ProcSimulator {
         for(int i = 0; i < procTime.length; i++)
         {
             procTime[i]  = (int) (Math.random()*(maxPrTime - minPrTime)) + minPrTime;
-            totalProcTime = totalProcTime + i;
         }
         //Arrays.sort(procTime);
         //System.out.println(Arrays.toString(procTime));
@@ -53,9 +68,18 @@ public class ProcSimulator {
         {
             arrTime[j] = j + 1;
         }
-        System.out.println(Arrays.toString(arrTime));    
+        //System.out.println(Arrays.toString(arrTime));    
         
+        for(int x = 0; x < arrTime.length; x++)
+        {
+            if(x%2 == 0)
+            {
+                proc1(arrTime[x], procTime[x]);
+            }
+            else{
+                proc2(arrTime[x], procTime[x]);
+            }
         
+        }
     }
-    
 }
